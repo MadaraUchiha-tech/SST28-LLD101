a06d231a1e5604a8adef3a9fc79ed58ae518bed9
navjeetsingh <haarjeet333@gmail.com>
2026-03-03 02:28:27 +0530

    Updated Base Contract

--- SOLID/Ex6/src/NotificationSender.java
+++ SOLID/Ex6/src/NotificationSender.java
@@ -1,5 +1,8 @@
 public abstract class NotificationSender {
     protected final AuditLog audit;
     protected NotificationSender(AuditLog audit) { this.audit = audit; }
+//      Base contract:
+//      Accept a non-null Notification.
+//      Do not throw for channel errors; log them instead.
     public abstract void send(Notification n);
 }
